ac3d24529be80ec7b94375c027680a1c3c60c2e9
root <root@DESKTOP-5I6DCC0.localdomain>
2020-10-27 14:33:52 -0300

    a

---
+++ main.c
@@ -0,0 +1,24 @@
+/* 
+1) Escolha um site de notícias que possui o recurso de feed RSS e faça o
+download de seu arquivo .xml;
+
+2) Crie um programa em C que recebe um arquivo RSS no formato .xml e
+extrai as seguintes informações:
+    a) Sobre o Canal
+
+        i) Título do canal
+        ii) Descrição do canal
+        iii) Link do canal
+
+    b) Sobre cada notícia (item)
+        i) Título da notícia
+        ii) Descrição da notícia
+        iii) Link (se disponível)
+        iv) Data de publicação se disponível
+
+3) Essas informações deverão ser salvas em um arquivo de formato .html
+com o seguinte padrão:
+*/
+int main(void)
+{
+}
